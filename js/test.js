358e76e532542d8a101c1c82cbc072ed6802113e
Allahuddin <amk17th@AMKs-MacBook-Air.local>
2024-02-22 22:21:07 +0430

    create h1 tag and we edit this and p tag

--- js/test.js
+++ js/test.js
@@ -1,9 +1,28 @@
-var x = 12;
+// var x = 12;
 
-document.write(x);
+// document.write(x);
 
 //how to create element 
 
-var y  = '<h1>heading </h1>'
+// var y  = '<h1>heading </h1>'
 
-document.write(y);
+// document.write(y);
+
+//create h1 tag 
+
+var title = document.createElement('h1');
+title.textContent = 'heading';
+
+//create p tag 
+
+var content = document.createElement('p');
+content.textContent = "how are you dear man are find i hope your find "
+
+
+//get div contianer 
+
+var container = document.getElementById('container');
+
+//include h1 into container 
+container.appendChild(title);
+container.appendChild(content);
